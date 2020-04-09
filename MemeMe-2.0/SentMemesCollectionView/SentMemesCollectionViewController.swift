47d58c256958fba7cf690e2d3761ce43cab65c56
aftiyah <abd2010ly@gmail.com>
2020-04-09 10:35:18 +0200

    Add Sent Memes Collection View Controller

---
+++ MemeMe-2.0/SentMemesCollectionView/SentMemesCollectionViewController.swift
@@ -0,0 +1,84 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe-2.0
+//
+//  Created by Abdalfattah Altaeb on 4/9/20.
+//  Copyright © 2020 Abdalfattah Altaeb. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesCollectionViewController: UICollectionViewController {
+
+    //MARK: Properties & Outlets
+
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let space: CGFloat
+        let dimension: CGFloat
+
+
+        if (UIDeviceOrientationIsPortrait(UIDevice.current.orientation)) {
+            space = 3.0
+            dimension = (view.frame.size.width - (2 * space)) / 3 //3 per line
+        } else {
+            space = 1.0
+            dimension = (view.frame.size.width - (1 * space)) / 5
+        }
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        //Set Navigation Controller & Tab Bar Controller Hidden Properties
+        navigationController?.isNavigationBarHidden = false
+        tabBarController?.tabBar.isHidden = false
+
+        collectionView?.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return Meme.count()
+    }
+
+    //MARK: UICollectionViews Delegates
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AppModel.memesCollectionCellReuseIdentifier, for: indexPath) as! SentMemesCollectionViewCell
+
+        let meme = Meme.getMemeStorage().memes[indexPath.item]
+
+        cell.updateCell(meme)
+
+        return cell
+
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+        //Get the object of MemeDetailViewController from the Storyboard
+        let memeDetail = self.storyboard?.instantiateViewController(withIdentifier: AppModel.memeDetailStoryboardIdentifier) as! MemeDetailViewController
+
+        //Pass the Meme Date
+        memeDetail.meme = Meme.getMemeStorage().memes[indexPath.row]
+
+        //Push to the scene
+        navigationController?.pushViewController(memeDetail, animated: true)
+    }
+}
